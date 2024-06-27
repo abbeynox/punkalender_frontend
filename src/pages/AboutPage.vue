33ae7de6ccba880713fd90f63fa04ea37f182f59
abbeynox <git@ywk.ch>
2024-06-27 11:12:05 +0200

    fix vue routes

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,10 +1,43 @@
 <template>
   <div>
     <h1>Über Punkalender</h1>
-    <p>coming soon!</p>
+    <p>Diese Website ist noch gaanz am Anfang der Entwicklung :-). Mein Ziel ist es, gerade kleinere Punk Events besser
+      zu pushen, auch ausserhalb der Chats.</p>
+    <p>Du möchtest dieses Projekt unterstützen, hast Fragen oder einen Fehler entdeckt? Kontaktiere uns gerne über
+      <el-popconfirm title="Mail kopieren?" confirm-button-text="Mail kopieren" @confirm="copyEmail">
+        <template #reference>
+          <a href="javascript:void(0)">admin@punkalender.ch</a>
+        </template>
+      </el-popconfirm>.
+    </p>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ElNotification } from 'element-plus'
+import 'element-plus/theme-chalk/el-notification.css'
 
-<style scoped></style>
+const email = 'admin@punkalender.ch'
+
+const copyEmail = () => {
+  navigator.clipboard.writeText(email).then(() => {
+    ElNotification({
+      title: 'Erfolg',
+      message: 'Mail kopiert',
+      type: 'success',
+    })
+  }).catch(err => {
+    ElNotification({
+      title: 'Fehler',
+      message: 'Fehler beim Kopieren der Mail',
+      type: 'error',
+    })
+  })
+}
+</script>
+
+<style scoped>
+a {
+  cursor: pointer;
+}
+</style>
